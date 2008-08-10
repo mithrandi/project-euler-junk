e11e0a57b20fb08319b10d2894b6686dd03be33f
Tristan Seligmann <mithrandi@mithrandi.net>
2008-08-10 12:19:21 +0000

    Add type declarations.

--- Euler.hs
+++ Euler.hs
@@ -3,17 +3,21 @@ module Euler where
 import Data.List (unfoldr, find, nub)
 import Control.Monad (filterM)
 
+merge :: (Ord t) => [t] -> [t] -> [t]
 merge (x:xs) (y:ys) | x < y     = x : merge xs (y:ys)
                     | x > y     = y : merge (x:xs) ys
                     | otherwise = x : merge xs ys
 merge xs     []     = xs
 merge []     ys     = ys
 
+primes :: [Integer]
 primes = sieve [2..]
     where sieve (p:rest) = p : sieve [n | n <- rest, mod n p /= 0]
 
+divides :: (Integral a) => a -> a -> Bool
 divides m n = n `mod` m == 0
 
+factorise :: Integer -> [Integer]
 factorise n = unfoldr factor n
               where factor 1 = Nothing
                     factor m = do f <- find (`divides` m) (takeWhile (<= m) primes)
