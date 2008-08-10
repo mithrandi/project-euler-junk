2f818434ee7e48ba0afcae5e9652a345d9c24dad
Tristan Seligmann <mithrandi@mithrandi.net>
2008-08-10 12:19:30 +0000

    Use STUArray to optimise sum.

--- problem0023.hs
+++ problem0023.hs
@@ -1,18 +1,21 @@
 import Data.List
 import qualified Data.Set as Set
+import Data.Array.Unboxed
+import Data.Array.ST
 
 import Euler
 
-isAbundant :: Integer -> Bool
 isAbundant n = sum (divisors n) > n
 
-abundants :: [Integer]
 abundants = filter isAbundant [1..28123]
 
-sums :: [Integer]
 sums = filter (<= 28123) [x + head xs | xs <- tails abundants, x <- xs]
 
-resultSum :: Integer
-resultSum = sum [1..28123] - Set.fold (+) 0 (Set.fromList sums)
+resultsArray = runSTUArray $ do rs <- newListArray (0, 28123::Int) ([0..28123] :: [Int])
+                                mapM_ (\x -> writeArray rs (fromInteger x :: Int) 0) sums
+                                return rs
 
-main = print resultSum
+--resultSum = sum [1..28123] - Set.fold (+) 0 (Set.fromList sums)
+resultSum = sum . elems $ resultsArray
+
+main = print $ resultSum == 4179871
