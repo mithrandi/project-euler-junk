b4418c46e6c5198f624115e3500d44fc144c464a
Tristan Seligmann <mithrandi@mithrandi.net>
2008-08-10 12:36:00 +0000

    Clean up code a bit.

--- problem0023.hs
+++ problem0023.hs
@@ -11,11 +11,11 @@ abundants = filter isAbundant [1..28123]
 
 sums = filter (<= 28123) [x + head xs | xs <- tails abundants, x <- xs]
 
-resultsArray = runSTUArray $ do rs <- newListArray (0, 28123::Int) ([0..28123] :: [Int])
-                                mapM_ (\x -> writeArray rs (fromInteger x :: Int) 0) sums
+resultsArray :: UArray Int Int
+resultsArray = runSTUArray $ do rs <- newListArray (0, 28123) [0..28123]
+                                mapM_ (\x -> writeArray rs (fromInteger x) 0) sums
                                 return rs
 
---resultSum = sum [1..28123] - Set.fold (+) 0 (Set.fromList sums)
 resultSum = sum . elems $ resultsArray
 
 main = print $ resultSum == 4179871
